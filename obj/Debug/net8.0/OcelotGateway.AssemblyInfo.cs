7e28e346f0af80f733817048bbaa700bef3de9f9
Efe Demirtaş <efedem21@hotmail.com>
2025-05-19 02:46:25 +0300

    v5

--- obj/Debug/net8.0/OcelotGateway.AssemblyInfo.cs
+++ obj/Debug/net8.0/OcelotGateway.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OcelotGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19005a0bb52ef729125d973a4f6a47eed2b1c905")]
 [assembly: System.Reflection.AssemblyProductAttribute("OcelotGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OcelotGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
